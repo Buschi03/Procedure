6f7f5aa25e98865b8bb445d72674880d879a42b1
Buschi <mattia.bus2003@gmail.com>
2023-08-24 23:26:58 +0200

    Update medicine.js

--- medicine.js
+++ medicine.js
@@ -1,3 +1,24 @@
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function resetCheckboxes() {
     var now = new Date();
     var hours = now.getHours();
@@ -7,20 +28,20 @@ function resetCheckboxes() {
         
         checkboxes.forEach(function(checkbox) {
             checkbox.checked = false;
-            localStorage.removeItem(checkbox.id); // Rimuovi il valore dalla localStorage
+            setCookie(checkbox.id, 'false', 365); // Imposta il cookie con stato "false"
         });
     }
 }
 
 function saveCheckboxState(checkboxId, isChecked) {
-    localStorage.setItem(checkboxId, isChecked); // Salva lo stato della checkbox nella localStorage
+    setCookie(checkboxId, isChecked.toString(), 365); // Imposta il cookie con lo stato della checkbox
 }
 
 function loadCheckboxStates() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
     
     checkboxes.forEach(function(checkbox) {
-        var isChecked = localStorage.getItem(checkbox.id) === 'true';
+        var isChecked = getCookie(checkbox.id) === 'true';
         checkbox.checked = isChecked;
     });
 }
